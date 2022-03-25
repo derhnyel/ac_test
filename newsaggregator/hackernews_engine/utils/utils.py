d4feeef825d9aca750d6523e28f34548ced757c4
Daniel <42381829+derhnyel@users.noreply.github.com>
2022-03-25 10:26:42 +0100

    Update utils.py

--- newsaggregator/hackernews_engine/utils/utils.py
+++ newsaggregator/hackernews_engine/utils/utils.py
@@ -1,12 +1,15 @@
 import re
 def word_frequency(sentences):
     wordcount_dict = {}
+    reg = re.compile(r'\w+')
     for sentence in sentences:
         for word in sentence.split():
-            if word in wordcount_dict and word:
-                wordcount_dict[word]=wordcount_dict[word]+1
-                continue 
-            wordcount_dict[word] = 1
+            word_regex_length = len(reg.findall(word))
+            if word_regex_length == 1:
+                if word in wordcount_dict:
+                    wordcount_dict[word]=wordcount_dict[word]+1
+                    continue 
+                wordcount_dict[word] = 1    
     wordcount_dict = sorted(wordcount_dict.items(),key =lambda x:x[1],reverse=True)                    
     return wordcount_dict   
 def check_query(value):
